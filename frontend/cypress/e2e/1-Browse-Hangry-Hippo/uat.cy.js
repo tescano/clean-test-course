6643245f6e8121918529c1fd988b011380e0fce7
tescano <tescano.work@gmail.com>
2026-08-10 20:38:30 -0400

    add UAT testing using Cypress

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,15 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click(); //verifies HomePage contains Handhelds, if so, click it.
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');// verifies Cheese Burger is not presented
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
